15a6a6570d9e3c56503f1101dbaaf6cf8f34b0c1
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2025-11-29 23:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (36.36%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/wiggle-subsequence.cpp
@@ -4,14 +4,14 @@ public:
          if(nums.size() < 2) return nums.size();
         int count=1;int dir=0;
         for(int i=1;i<nums.size();i++){
-            int ans=nums[i-1]-nums[i];
+            int ans=nums[i-1]-nums[i];//differnce lena hai right-left
             if(ans>0&&dir<=0){
             count++;
-            dir=1;//direction is up
+            dir=1;//direction postive
         }
         else if(ans<0&& dir>=0){
             count++;
-            dir=-1;
+            dir=-1;//direction negative
         }
         }
         return count;
