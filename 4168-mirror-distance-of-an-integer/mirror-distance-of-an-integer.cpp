bf309b2fa5d027a942d5bffcb7cb315aacc4c0df
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2025-12-21 10:45:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (45.45%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int temp=n;
+        int ans=0;
+       while (n > 0) {
+            ans = ans*10+(n % 10);
+            n /= 10;
+        }
+        return abs(temp-ans);
+    }
+};
