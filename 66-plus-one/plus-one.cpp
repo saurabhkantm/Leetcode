d468775488375f7dbc6c0c0f573b16eb2ee0ed94
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-01-01 10:34:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (77.72%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,15 +1,18 @@
 class Solution {
- public:
-  vector<int> plusOne(vector<int>& digits) {
-    for (int i = digits.size() - 1; i >= 0; --i) {
-      if (digits[i] < 9) {
-        ++digits[i];
-        return digits;
-      }
-      digits[i] = 0;
+public:
+        vector<int> plusOne(vector<int>& digits) {
+    int n = digits.size();
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+        digits[i] = 0;
     }
 
-    digits.insert(digits.begin(), 1);
+    digits.insert(digits.begin(), 1);//insert krega digits ko
     return digits;
-  }
+
+    }
 };
