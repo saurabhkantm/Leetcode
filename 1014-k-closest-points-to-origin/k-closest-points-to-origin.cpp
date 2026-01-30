a05f82118e4d27832929ab4c6bc724e8d10e37ea
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-01-31 00:15:18 +0530

    Time: 84 ms (34.30%) | Memory: 77.9 MB (35.15%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>> maxHeap;
-        vector<vector<int>> ans;
-        
-        for(int i=0;i<k;i++){
-            int x = points[i][0];
-            int y = points[i][1];
-
-            int ele = x*x + y*y;
-            maxHeap.push({ele, i});
-        }
-
-        for(int i=k;i<points.size();i++){
-            int x = points[i][0];
-            int y = points[i][1];
-            int ele = x*x + y*y;
-
-            if(ele < maxHeap.top().first){
-                maxHeap.pop();
-                maxHeap.push({ele, i});
+        //max heap
+        priority_queue<pair<int,vector<int>>>pq;
+        for(int i=0;i<points.size();i++){
+            int dist= points[i][0]*points[i][0] + points[i][1]*points[i][1];
+            pq.push({dist,points[i]});
+            if(pq.size()>k){
+                pq.pop();
             }
         }
-
-        while(!maxHeap.empty()){
-            int i = maxHeap.top().second;
-            maxHeap.pop();
-            ans.push_back({points[i][0], points[i][1]});
+        vector<vector<int>>ans;
+        while(!pq.empty()){
+      ans.push_back(pq.top().second);
+      pq.pop();
         }
-
         return ans;
     }
 };
+        
