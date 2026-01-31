1983ec61ed0a03a2fa5a164563731a122df5e355
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-01-31 23:45:43 +0530

    Time: 67 ms (9.36%) | Memory: 49.9 MB (18.10%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int solve(int index, vector<int>& coins, int amount, vector<vector<int>>& dp) {
-        if (amount == 0) return 0;
-        if (amount < 0 || index == coins.size()) return 1e9;
-
-        if (dp[index][amount] != -1)
-            return dp[index][amount];
-
-        int pick = 1e9, NotPick = 1e9;
-
-        if (coins[index] <= amount) {
-            pick = 1 + solve(index, coins, amount - coins[index], dp);
-        }
-
-        NotPick = solve(index + 1, coins, amount, dp);
-
-        return dp[index][amount] = min(pick, NotPick);
+int solve(vector<int>&coins,int amount,int i,vector<vector<int>>&dp){
+    //base case
+    if(i>=coins.size()){
+        if(amount==0)
+            return 0;
+        else
+            return INT_MAX/2;
     }
+    if(dp[i][amount]!=-1) return dp[i][amount];
+    //notpick
+    int notpick=solve(coins,amount,i+1,dp);
+    //pick
+    int pick=INT_MAX/2;
+    if(coins[i]<=amount){
+     pick=solve(coins,amount-coins[i],i,dp)+1;
+    }
+    return dp[i][amount]=min(pick,notpick);
 
+}
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-
-        int ans = solve(0, coins, amount, dp);
-        return (ans >= 1e9 ? -1 : ans);
+        int n=coins.size();
+    vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        int ans= solve(coins,amount,0,dp);
+        if(ans>=INT_MAX/2)
+        return -1;
+        return ans;
     }
 };
