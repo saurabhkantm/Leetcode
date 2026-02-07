06408c17648c4ebaf0f4ffc439f90ce9244c4c1b
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-02-07 23:06:02 +0530

    Time: 38 ms (34.32%) | Memory: 26.3 MB (44.82%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int res = 0, b = 0;
-
-        for (auto& c : s) {
-            if (~c & 1) b++;
-            else if (b) {
-                res++;
-                b--;
+        stack<char> st;
+        int cnt = 0;
+        for(int i=0;i<s.size();i++){
+            if(st.empty() || s[i] == 'b'){
+                st.push(s[i]);
+            }
+            else if(s[i] == 'a' && st.top() == 'a'){
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+                cnt++;
             }
         }
 
-        return res;
+        return cnt;
     }
 };
