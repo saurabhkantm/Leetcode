d199f9f07983dd7bd681dc3c3d351caba3384958
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-02-24 22:21:00 +0530

    Time: 14 ms (14.65%) | Memory: 19.6 MB (17.77%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+int solve(vector<int>&nums,int i,int j,vector<vector<int>>&dp){
+        if(i==j){
+            return nums[i];
+        }
+        if(i>j){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int take1 = nums[i] + min(solve(nums,i+2,j,dp),solve(nums,i+1,j-1,dp));
+        int take2 = nums[j] + min(solve(nums,i+1,j-1,dp),solve(nums,i,j-2,dp));
+
+        return dp[i][j] = max(take1,take2);
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+         int n = piles.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        int first = solve(piles,0,n-1,dp);
+        int sum = 0;
+        // cout<<first<<endl;
+        for(int i=0;i<piles.size();i++){
+            sum += piles[i];
+        }
+        int second = sum - first;
+        // cout<<second<<endl;
+        if(first>=second){
+            return true;
+        }
+        return false;
     }
 };
