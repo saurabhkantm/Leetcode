25cf07e2ee731d6bdf7ed030c33660f7ff0bb1d4
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-02-25 08:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (92.86%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    static bool comp(int a,int b){
-        int first = 0;
-        int second = 0;
-        int tempA = a;
-        int tempB = b;
-        while(a>0){
-            first += (a & 1);
-            a = a>>1;
-        }
-        while(b>0){
-            second += (b & 1);
-            b = b>>1;
-        }
-        // cout<<"a:"<<tempA<<endl;
-        // cout<<"b:"<<tempB<<endl;
-        // cout<<first<<endl;
-        // cout<<second<<endl;
+    static bool comp(int a, int b){
+        int first = __builtin_popcount(a);
+        int second = __builtin_popcount(b);
+        
         if(first == second)
-            return tempA < tempB;
-        return first<second;
+            return a < b;
+        
+        return first < second;
     }
+
     vector<int> sortByBits(vector<int>& arr) {
-        sort(arr.begin(),arr.end(),comp);
+        sort(arr.begin(), arr.end(), comp);
         return arr;
     }
 };
