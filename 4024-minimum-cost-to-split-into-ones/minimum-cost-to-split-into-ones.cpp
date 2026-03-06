ab7050ad7da58fc285e6b995fcd0e643ee2e730e
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-03-06 11:56:14 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (91.47%) - LeetSync

--- 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
+++ 4024-minimum-cost-to-split-into-ones/minimum-cost-to-split-into-ones.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int minCost(int n) {
-        vector<long long> dp(n + 1, 0);
-        dp[1] = 0;
-        for (int x = 2; x <= n; x++) {
-            dp[x] = LLONG_MAX;
-            for (int a = 1; a < x; a++) {
-                long long cost = 1LL * a * (x - a) + dp[a] + dp[x - a];
-                dp[x] = min(dp[x], cost);
-            }
-        }
-        return dp[n];
+        return n * (n - 1) / 2;
     }
 };
