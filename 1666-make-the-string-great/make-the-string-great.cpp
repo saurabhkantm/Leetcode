3f09c1b3600ce5ce3df9bcf58404e012ce11be18
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-04-11 00:13:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (96.79%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string makeGood(string s) {
+       string st;
+    for (char c : s) {
+        if (!st.empty()&&abs(st.back()-c)==32) st.pop_back();
+        else st+=c;
+    }
+    return st; 
+    }
+};
