690887d5a6c46e0ec052defccf255da12708d3b7
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-04-26 00:41:11 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (15.19%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -3,12 +3,8 @@ public:
     void reverseString(vector<char>& s) {
         int i=0,j=s.size()-1;
         while(i<j){
-          swap(s[i],s[j]);
+            swap(s[i],s[j]);
             i++,j--;
         }
     }
 };
-
-
-//ha
-//ah
