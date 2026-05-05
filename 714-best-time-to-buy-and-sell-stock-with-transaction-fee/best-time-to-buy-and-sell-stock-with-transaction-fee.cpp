9e651b2b0792ec99f9d50f518bb215a5836f43f4
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-05-06 02:10:25 +0530

    Time: 135 ms (5.32%) | Memory: 109.4 MB (5.45%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-int solve(int i,bool isbuy,vector<int>& prices,int fee,vector<vector<int>>&dp){
-    //base case
-    if(i>=prices.size()) return 0;
-    if(dp[i][isbuy]!=-1) return dp[i][isbuy];
-    //nottake
-    int nottake=solve(i+1,isbuy,prices,fee,dp);
-    //take
-    int take=0;
-    if(isbuy==false){
-        take=-prices[i]+solve(i+1,true,prices,fee,dp);
+int solve(int i,bool buy ,vector<int>& prices,int fee, vector<vector<int>>&dp){
+    if(i>=prices.size()){
+        return 0;
+    }
+    if(dp[i][buy]!=-1) return dp[i][buy];
+    int nottake=0,take=0;
+    nottake=solve(i+1,buy,prices,fee,dp);
+    if(buy==0){
+        take=solve(i+1,1,prices,fee,dp)-prices[i];
     }
     else{
-        take=-fee+prices[i]+solve(i+1,false,prices,fee,dp);
+        take=solve(i+1,0,prices,fee,dp)+prices[i]-fee;
     }
-    return dp[i][isbuy]=max(take,nottake);
+    return dp[i][buy]=max(take,nottake);
 }
     int maxProfit(vector<int>& prices, int fee) {
         int n=prices.size();
         vector<vector<int>>dp(n,vector<int>(2,-1));
-        return solve(0,false,prices,fee,dp);
+        return solve(0,0,prices,fee,dp);
     }
 };
