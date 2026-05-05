d3485102db4cca14299499cb1960f8a5fc40ba3f
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-05-05 23:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (50.65%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-int solve(int idx,vector<int>&nums,vector<int>&dp){
-   // int n=nums.size();
-    //base case
-    if(idx>=nums.size()) return 0;
+    int maxRob(int i, int n, vector<int>& nums, vector<int>& dp){
+        if(i > n)
+        return 0;
+        if(dp[i]!=-1)
+        return dp[i];
 
-    if(dp[idx]!=-1) return dp[idx];
-    //not take
-    int nottake=solve(idx+1,nums,dp);
-    //take
-    int take=nums[idx]+solve(idx+2,nums,dp);
-    return dp[idx]=max(take,nottake);
-}
+        return dp[i] = max(maxRob(i+1, n, nums, dp), (nums[i] + maxRob(i+2, n, nums, dp)));
+    }
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>dp(n+1,-1);
-        //base case
-        if(n==1) return nums[0];
-        vector<int>nums1;
-        vector<int>nums2;
-        for(int i=0;i<=n-2;i++){
-        nums1.push_back(nums[i]);
-        }
-        for(int i=1;i<=n-1;i++){
-        nums2.push_back(nums[i]);
-        }
-        int ans1= solve(0,nums1,dp);
-        for(int i=0;i<=n;i++){
-            dp[i]=-1;
-        }
-          int ans2=solve(0,nums2,dp);
-          return max(ans1,ans2);
+        int n = nums.size();
+
+        if(n == 1)
+        return nums[0];
+
+        if(n == 2)
+        return max(nums[0], nums[1]);
+
+        vector<int> dp(n, -1);
+        vector<int> dp2(n, -1);
+        return max(maxRob(0, n-2, nums, dp), maxRob(1, n-1, nums, dp2));
     }
 };
-
