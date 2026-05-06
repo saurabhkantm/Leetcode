10149920226771a3603f2c2497fddbc7d8c8f51e
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-05-06 14:19:23 +0530

    Time: 26 ms (64.33%) | Memory: 27.6 MB (26.49%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,18 +1,59 @@
 class Solution {
 public:
-int solve(int i,int j,string &text1,string &text2, vector<vector<int>>&dp){
-   if(i==0||j==0) return 0;
-   if(dp[i][j]!=-1) return dp[i][j];
-   if(text1[i-1]==text2[j-1]){
-    return dp[i][j]=1+solve(i-1,j-1,text1,text2,dp);
-   }
-   else{
-    return dp[i][j]=max(solve(i,j-1,text1,text2,dp),solve(i-1,j,text1,text2,dp));
-   }
-}
+
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size(),m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(n,m,text1,text2,dp);
+
+        int m = text1.size();
+        int n = text2.size();
+
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+
+        for(int i=1;i<=m;i++){
+
+            for(int j=1;j<=n;j++){
+
+                if(text1[i-1] == text2[j-1]){
+
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+
+                    dp[i][j] = max(dp[i-1][j],
+                                   dp[i][j-1]);
+                }
+            }
+        }
+
+        // Print LCS
+        string lcs = "";
+
+        int i = m;
+        int j = n;
+
+        while(i>0 && j>0){
+
+            if(text1[i-1] == text2[j-1]){
+
+                lcs.push_back(text1[i-1]);
+
+                i--;
+                j--;
+            }
+            else{
+
+                if(dp[i-1][j] > dp[i][j-1]){
+                    i--;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+
+        reverse(lcs.begin(), lcs.end());
+
+        cout << lcs << endl;
+
+        return dp[m][n];
     }
 };
