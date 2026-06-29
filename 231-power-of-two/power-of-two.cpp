27661f74557554ceb6fd7312d81b3b90c3a213ef
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-06-29 10:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (11.98%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) return false;
-
-        if((n & (n - 1)) == 0)
-            return true;
-        else
-            return false;
+        for(int i=0;i<=30;i++){
+            int ans=pow(2,i);
+            if(ans==n){
+                return true;
+            }
+        }
+        return false;
     }
 };
