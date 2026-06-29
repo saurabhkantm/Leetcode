61ec2551a88c9147bab85cb9aad711892ffa2527
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-06-29 10:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (25.56%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     int bitwiseComplement(int n) {
-        if(n == 0) return 1;
-        string binary = "";
-
-        while(n > 0) {
-            int digit = n % 2;
-            binary = (digit == 0 ? "0" : "1") + binary;
-            n = n / 2;
-        }
-
-        for(int i = 0; i < binary.size(); i++) {
-            if(binary[i] == '0') binary[i] = '1';  
-            else binary[i] = '0';
-        }
-
-        int ans = 0;
-        for(char c : binary) ans = ans * 2 + (c - '0');
-        return ans;
+        if(n==0) return 1;
+       int x=1;
+       while(x<=n){
+        n=n^x;
+        x=x<<1;
+       }
+       return n;
     }
 };
