b90eb9ef3869a9f63b7ac7d18ae647f4b34bd297
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-07-07 01:06:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.67%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
+int climb(int n,vector<int>&dp){
+    if(n<=2){
+        return n;
+    }
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]= climb(n-1,dp)+climb(n-2,dp);
+}
     int climbStairs(int n) {
-        //base case
         vector<int>dp(n+1,-1);
-        if(n<=1) return 1;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+       return  climb(n,dp);
     }
 };
