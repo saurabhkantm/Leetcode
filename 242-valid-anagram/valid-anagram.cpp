6fb7e6ca6dcd4242bbc43af327183270907e83ea
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-07-18 18:16:43 +0530

    Time: 7 ms (18.70%) | Memory: 9.5 MB (99.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
         sort(s.begin(),s.end());
-         sort(t.begin(),t.end());
+        sort(t.begin(),t.end());
         if(s==t){
-        return true;
-    }
-    return false;
+            return true;
+        }
+        return false;
     }
 };
