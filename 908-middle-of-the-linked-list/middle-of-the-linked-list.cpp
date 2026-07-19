7ac5d79ec5c788f32b339d60905286718b96dbfc
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-07-19 13:04:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (26.88%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -3,11 +3,10 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode*slow=head;
         ListNode*fast=head;
-        //fast do step chale ga aur slow ek step
-        while(fast!=nullptr&&fast->next!=nullptr){
-        slow=slow->next;
-        fast=fast->next->next;
-    }
-    return slow;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
     }
 };
