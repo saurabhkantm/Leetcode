8a9c25683410e7ee74ae3f5244b4d1e4783f1343
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-07-19 12:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (72.41%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-       vector<int>ans;
-       ListNode*curr=head;
-       while(curr){
-       ans.push_back(curr->val);
-       curr=curr->next;
-       }
-       curr=head;
-       for(int i=ans.size()-1;i>=0;i--){
-        curr->val=ans[i];
-        curr=curr->next;
-       }
-       return head;
+    //    vector<int>ans;
+    //    ListNode*curr=head;
+    //    //store value
+    //    while(curr){
+    //    ans.push_back(curr->val);
+    //    curr=curr->next;
+    //    }
+    //    //traverse again
+    //    curr=head;
+    //    // Step 3: Replace values in reverse order
+    //    for(int i=ans.size()-1;i>=0;i--){
+    //     curr->val=ans[i];
+    //     curr=curr->next;
+    //    }
+    //    return head;
+    ListNode*prev=nullptr;
+    ListNode*curr=head;
+    while(curr){
+        ListNode*next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
     }
 };
