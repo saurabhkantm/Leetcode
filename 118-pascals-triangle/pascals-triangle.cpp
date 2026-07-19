de3aeea6e4298444e2940d5a16f2080e6ed9a718
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-07-19 16:42:36 +0530

    Time: 3 ms (7.91%) | Memory: 9.6 MB (63.97%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+int ncr(int n,int r){
+    int s=1;
+    for(int i=0;i<r;i++){
+        s*=(n-i);
+        s/=i+1;
+    }
+    return s;
+}
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> ans;
-
-        for(int i = 0; i < numRows; i++) {
-
-            ans.push_back(vector<int>(i + 1, 1));
-
-            for(int j = 1; j < i; j++) {
-                ans[i][j] = ans[i-1][j-1] + ans[i-1][j];
+        //brute force
+        int n=numRows;
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i++){
+            vector<int>colRows;
+            for(int j=0;j<=i;j++){
+                colRows.push_back(ncr(i,j));
             }
+            ans.push_back(colRows);
         }
-
-        return ans;
+   return ans;
     }
 };
