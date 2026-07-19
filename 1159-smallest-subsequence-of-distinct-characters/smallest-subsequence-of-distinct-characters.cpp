05b9a289ae0ad348e1f7e6c1c48c71154b8c0f0e
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-07-19 15:01:26 +0530

    Time: 2 ms (17.88%) | Memory: 8.6 MB (79.99%) - LeetSync

---
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+         unordered_map<char, int> freq;
+        vector<bool> inAns(26, false);
+        for(char ch: s) freq[ch]++;
+
+        string ans;
+        for(char ch:s){
+            if(!inAns[ch-'a']){
+                while(!ans.empty() && ans.back() > ch){
+                    if(freq[ans.back()] > 0){
+                        inAns[ans.back()-'a'] = false;
+                        ans.pop_back();
+                    }
+                    else break;
+                }
+                inAns[ch-'a'] = true;
+                ans.push_back(ch);
+            }
+            freq[ch]--;
+        }
+        return ans;
+    }
+};
