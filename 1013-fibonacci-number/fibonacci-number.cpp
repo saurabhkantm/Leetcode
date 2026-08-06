242dcff035e2453ea60b50228c6eef074f82df47
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-08-06 21:58:48 +0530

    Time: 2 ms (77.84%) | Memory: 7.7 MB (65.80%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
+// int solve(int n,vector<int>&dp){
+//     //base case
+//     //if(n<=1) return n;
+//     if(n==0) return 0;
+//     if(n==1) return 1;
+//     if(dp[n]!=-1) return dp[n];//store
+//      return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+// }
     int fib(int n) {
-        //recursion approxhes
-        //base case
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n<=1) return n;
+    //bottom approches
+    //space optimization 
+    int a=0,b=1,c;
+    for(int i=2;i<=n;i++){
+        c=a+b;
+        a=b;
+        b=c;
+    } 
+    return c;
     }
 };
-
