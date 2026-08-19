9e751ac64ac55e918c7afb8c3a29de993fc05ef3
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-08-19 13:09:21 +0530

    Time: 2635 ms (5.01%) | Memory: 63 MB (8.91%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-bool ispalindrome(int i,int j,string &s){
+ bool palindrome(int i,int j,string &s){
     while(i<j){
-        if(s[i]!=s[j]){
+    if(s[i]!=s[j]) 
         return false;
-        }
-        i++;j--;
+    i++;j--;
     }
     return true;
-}
-int solve(int i,int j,string &s, vector<vector<int>>&dp){
+ }
+ int solve(int i,int j,string &s,vector<vector<int>>&dp){
     //base case
     if(i>=j) return 0;
     if(dp[i][j]!=-1) return dp[i][j];
+    if(palindrome(i,j,s)) return 0;
     int ans=INT_MAX;
-    if(ispalindrome(i,j,s)) return 0;
     for(int k=i;k<j;k++){
-        if(ispalindrome(i,k,s)){
-            int cut=1+solve(k+1,j,s,dp);
-            ans=min(ans,cut);
-    }
+        if(palindrome(i,k,s)){
+            int cost=1+solve(k+1,j,s,dp)+solve(i,k,s,dp);
+            ans=min(ans,cost);
+        }
     }
     return dp[i][j]=ans;
-}
-
+ }
     int minCut(string s) {
         int n=s.size();
         vector<vector<int>>dp(n,vector<int>(n,-1));
-     return solve(0,n-1,s,dp);   
+        return solve(0,n-1,s,dp);
     }
 };
