a001b1e03edbbe9ac7315884821b67660c9b10ec
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-08-19 15:26:08 +0530

    Time: 43 ms (8.48%) | Memory: 13.9 MB (54.61%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-  int solve(int i,int j,vector<int>&cuts,vector<vector<int>>&dp){
+int solve(int i,int j,vector<int>& cuts,vector<vector<int>>&dp){
     //base case
-    //i aur j adjacent hain => beech mein koi cut nahi
     if(i+1==j) return 0;
     if(dp[i][j]!=-1) return dp[i][j];
-       int mini = INT_MAX;
+    int ans=INT_MAX;
     for(int k=i+1;k<j;k++){
-        int cost=cuts[j]-cuts[i]+solve(i,k,cuts,dp)+solve(k,j,cuts,dp);
-        mini=min(mini,cost);
+        int cost=solve(i,k,cuts,dp)+solve(k,j,cuts,dp)+cuts[j]-cuts[i];
+        ans=min(ans,cost);
     }
-return dp[i][j]=mini;
-  }
+    return dp[i][j]=ans;
+}
     int minCost(int n, vector<int>& cuts) {
-         cuts.push_back(0);
-         cuts.push_back(n);
-         int m=cuts.size();
-         sort(cuts.begin(),cuts.end());
-         vector<vector<int>>dp(m,vector<int>(m,-1));
-          return solve(0,m-1,cuts,dp);
+        //code
+        sort(cuts.begin(),cuts.end());
+        cuts.insert(cuts.begin(),0);
+        cuts.push_back(n);
+        int m=cuts.size();
+        vector<vector<int>>dp(m,vector<int>(m,-1));
+        return solve(0,m-1,cuts,dp);  
     }
 };
