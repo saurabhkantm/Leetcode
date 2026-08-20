ee3babe40b07cf4bcb73a1c80986670673359170
Saurabh Kant Mishra <152972330+saurabhkantm@users.noreply.github.com>
2026-08-20 14:23:14 +0530

    Time: 16 ms (9.60%) | Memory: 27.9 MB (5.16%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(int i=0;i<=n;i++){
+            if(mp[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
